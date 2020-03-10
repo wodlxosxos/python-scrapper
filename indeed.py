2ca17d231259958354207a84576564e58a0ab79a
wodlxosxos <43673102+wodlxosxos@users.noreply.github.com>
2020-03-11 02:08:05 +0900

    indeed_extracting
    extract부분까지

---
+++ indeed.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+LIMIT = 50
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}&radius=25"
+
+def extract_indeed_pages():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.string))
+    max_page = pages[-1]
+    return max_page
+
+def extract_job(html):
+    title = html.find("div", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    return {'title': title,'company': company}
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+    return jobs
